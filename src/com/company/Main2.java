e40711555a2972cdbb1f01a3b81fbb6bb25ef147
makeev1487 <makeev1487@gmail.com>
2021-10-17 20:34:03 +0300

    Init

--- src/com/company/Main2.java
+++ src/com/company/Main2.java
@@ -3,5 +3,8 @@ package com.company;
 public class Main2 {
     public static void main(String[] args) {
         System.out.print("I made it!!!");
+        System.out.print("Hello, world!!!");
+
+
     }
 }
